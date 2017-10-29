192ded373eec33ea00f0749461baa5676125bf51
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-10-29 22:59:21 +0100

    Add Items

---
+++ CashConveyorBeltSimulator/Items/Apple.cs
@@ -0,0 +1,22 @@
+﻿using CashConveyorBeltSimulator.Interfaces;
+using engenious;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashConveyorBeltSimulator.Items
+{
+    class Apple : BaseItem, IMoveable
+    {
+        public Apple(int x, int y)
+        {
+            Position = new Point(x, y);
+            Name = nameof(Apple);
+        }
+
+        public Point AddToPosition(int x, int y) => Position = new Point(Position.X + x, Position.Y + y);
+
+    }
+}
---
+++ CashConveyorBeltSimulator/Items/BaseItem.cs
@@ -0,0 +1,17 @@
+﻿using engenious;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashConveyorBeltSimulator.Items
+{
+    abstract class BaseItem
+    {
+        public Point Position { get; set; }
+        public string Name { get; internal set; }
+
+        public Rectangle CollisionBox { get; internal set; }
+    }
+}
